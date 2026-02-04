b3a5d539723fa8318e4db92a078aea95743b6161
cz <czy190824@hotamil.com>
2026-02-05 00:23:41 +0800

    fix(config): 更新Umami脚本的URL

--- src/config.ts
+++ src/config.ts
@@ -685,6 +685,6 @@ export const umamiConfig = {
 	apiKey: import.meta.env.UMAMI_API_KEY || "api_xxxxxxxx", // API密钥优先从环境变量读取，否则使用配置文件中的值
 	baseUrl: import.meta.env.UMAMI_BASE_URL || "https://api.umami.is", // Umami API地址优先从环境变量读取
 	scripts: `
-<script defer src="XXXX.XXX" data-website-id="ABCD1234"></script>
+<script defer src="https://um.txbots.dpdns.org/script.js" data-website-id="ef5f184f-c779-4b69-9ba2-453cf4f85345"></script>
   `.trim(), // 上面填你要插入的Script,不用再去Layout中插入
 } as const;
